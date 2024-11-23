c893d6b3f4ecad06a2e76b88bf1086e053c81852
rafaelseijinm <rafaelseiji6@gmail.com>
2024-11-23 10:21:26 -0300

    corrigindo home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,16 +10,16 @@ function Home() {
       <nav>
         <ul>
           <li>
-            <Link to="/Atv01">Atividade 1</Link>
+            <Link to="/Atividade1">Atividade 1</Link>
           </li>
           <li>
-            <Link to="/Atv02">Atividade 2</Link>
+            <Link to="/Atividade2">Atividade 2</Link>
           </li>
           <li>
-            <Link to="/Atv03">Atividade 3</Link>
+            <Link to="/Atividade3">Atividade 3</Link>
           </li>
           <li>
-            <Link to="/Atv04">Atividade 4</Link>
+            <Link to="/Atividade4">Atividade 4</Link>
           </li>
         </ul>
       </nav>
