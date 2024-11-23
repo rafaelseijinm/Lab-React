157472d595f52e40fce998c45533cdb1f04b9ac8
rafaelseijinm <rafaelseiji6@gmail.com>
2024-11-23 11:00:47 -0300

    corrigindo imgs

--- src/components/Atividade2.js
+++ src/components/Atividade2.js
@@ -1,48 +1,23 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Atv2.css';
-
-function Atv2() {
-  const [contadorHomens, setContadorHomens] = useState(0);
-  const [contadorMulheres, setContadorMulheres] = useState(0);
-
-  const aumentarHomens = () => setContadorHomens(contadorHomens + 1);
-  const reduzirHomens = () => setContadorHomens(contadorHomens > 0 ? contadorHomens - 1 : 0);
-  const aumentarMulheres = () => setContadorMulheres(contadorMulheres + 1);
-  const reduzirMulheres = () => setContadorMulheres(contadorMulheres > 0 ? contadorMulheres - 1 : 0);
-
-  const zerarContadores = () => {
-    setContadorHomens(0);
-    setContadorMulheres(0);
-  };
-
-  return (
-    <div className="app-contador">
-      <h2>Atividade 02: Contador de Pessoas</h2>
-      <div className="contagem-total">
-        <h3>Total</h3>
-        <input type="text" value={contadorHomens + contadorMulheres} readOnly />
-        <button onClick={zerarContadores}>🔄</button>
-      </div>
-      <div className="secao-contadores">
-        <div className="contador-individual">
-          <h4>Homens</h4>
-          <img src="/imagens/homem01.png" alt="Homem" />
-          <button onClick={aumentarHomens}>+</button>
-          <span>{contadorHomens}</span>
-          <button onClick={reduzirHomens}>-</button>
-        </div>
-        <div className="contador-individual">
-          <h4>Mulheres</h4>
-          <img src="/imagens/mulher01.png" alt="Mulher" />
-          <button onClick={aumentarMulheres}>+</button>
-          <span>{contadorMulheres}</span>
-          <button onClick={reduzirMulheres}>-</button>
-        </div>
-      </div>
-      <Link to="/">Voltar</Link>
+<div class="container">
+  <h2>Homens</h2>
+  <div class="counter-section">
+    <img src="imagens/homem01.png" alt="Homens"></img>
+    <div class="counter-buttons">
+      <button class="increment">+</button>
+      <span>0</span>
+      <button class="decrement">-</button>
     </div>
-  );
-}
+  </div>
 
-export default Atv2;
+  <h2>Mulheres</h2>
+  <div class="counter-section">
+    <img src="imagens/mulher01.png" alt="Mulheres"></img>
+    <div class="counter-buttons">
+      <button class="increment">+</button>
+      <span>0</span>
+      <button class="decrement">-</button>
+    </div>
+  </div>
+
+  <button class="return-button">Voltar</button>
+</div>
