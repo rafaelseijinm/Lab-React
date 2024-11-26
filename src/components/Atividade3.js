648a311b289b8acec2130ccd9be35b70789b81de
rafaelseijinm <rafaelseiji6@gmail.com>
2024-11-26 16:15:11 -0300

    update atv3

--- src/components/Atividade3.js
+++ src/components/Atividade3.js
@@ -21,26 +21,5 @@ function Atv3() {
     </div>
   );
 }
-export default function Atividade4() {
-  return (
-      <div style={{ padding: "20px" }}>
-          <h1>Atividade 4 - Componentes</h1>
 
-          <h2>Exemplo 1: Galeria de Perfis</h2>
-          <Gallery />
-
-          <h2>Exemplo 2: Renderização Condicional</h2>
-          <ConditionalRendering isVisible={true} />
-
-          <h2>Exemplo 2: Renderização de Lista</h2>
-          <ListRendering />
-
-          <h2>Exemplo 3: JSX</h2>
-          <JSXExamples />
-
-          <h2>Exemplo 3: Props</h2>
-          <PropsExample />
-      </div>
-  );
-}
 export default Atv3;
