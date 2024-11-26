bf89bf57aac7e76c8448a53b3ccb4dd368e0e0cf
rafaelseijinm <rafaelseiji6@gmail.com>
2024-11-26 15:41:54 -0300

    update atv2

--- src/components/Atividade2.js
+++ src/components/Atividade2.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Atv2.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Atv2.css";
 
 function Atv2() {
   const [contadorHomens, setContadorHomens] = useState(0);
   const [contadorMulheres, setContadorMulheres] = useState(0);
 
   const aumentarHomens = () => setContadorHomens(contadorHomens + 1);
-  const reduzirHomens = () => setContadorHomens(contadorHomens > 0 ? contadorHomens - 1 : 0);
+  const reduzirHomens = () =>
+    setContadorHomens(contadorHomens > 0 ? contadorHomens - 1 : 0);
   const aumentarMulheres = () => setContadorMulheres(contadorMulheres + 1);
-  const reduzirMulheres = () => setContadorMulheres(contadorMulheres > 0 ? contadorMulheres - 1 : 0);
+  const reduzirMulheres = () =>
+    setContadorMulheres(contadorMulheres > 0 ? contadorMulheres - 1 : 0);
 
   const zerarContadores = () => {
     setContadorHomens(0);
@@ -17,29 +19,41 @@ function Atv2() {
   };
 
   return (
-    <div className="app-contador">
+    <div className="app-contador-atv2">
       <h2>Atividade 02: Contador de Pessoas</h2>
-      <div className="contagem-total">
+      <div className="contagem-total-atv2">
         <input type="text" value={contadorHomens + contadorMulheres} readOnly />
         <button onClick={zerarContadores}>🔄</button>
       </div>
-      <div className="secao-contadores">
-        <div className="contador-individual">
+      <div className="secao-contadores-atv2">
+        <div className="contador-individual-atv2">
           <h4>Homens</h4>
           <img src="/imagens/homem01.png" alt="Homem" />
           <button onClick={aumentarHomens}>+</button>
           <span>{contadorHomens}</span>
-          <button onClick={reduzirHomens} disabled={contadorHomens === 0}>-</button>
+          <button
+            onClick={reduzirHomens}
+            disabled={contadorHomens === 0}
+          >
+            -
+          </button>
         </div>
-        <div className="contador-individual">
+        <div className="contador-individual-atv2">
           <h4>Mulheres</h4>
           <img src="/imagens/mulher01.png" alt="Mulher" />
           <button onClick={aumentarMulheres}>+</button>
           <span>{contadorMulheres}</span>
-          <button onClick={reduzirMulheres} disabled={contadorMulheres === 0}>-</button>
+          <button
+            onClick={reduzirMulheres}
+            disabled={contadorMulheres === 0}
+          >
+            -
+          </button>
         </div>
       </div>
-      <Link to="/">Voltar</Link>
+      <Link to="/" className="atv2-link">
+        Voltar
+      </Link>
     </div>
   );
 }
